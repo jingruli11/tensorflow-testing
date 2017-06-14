4a5386f748c9301e472ff018513285a79a67cba9
Michael Li <lijingru931102@gmail.com>
2017-06-14 13:23:51 -0400

    commit 2 on test 4

--- test4.py
+++ test4.py
@@ -10,27 +10,28 @@ import sklearn
 df = pd.read_csv('dashride_columbia_dataset.csv')
 
 # split the string in starting and ending location to create coordinates variables
-df['start_lat'] = df['startLoc'].apply(lambda x: x.split(';')[0])
-df['start_long'] = df['startLoc'].apply(lambda x: x.split(';')[1])
-df['end_lat'] = df['endLoc'].apply(lambda x: x.split(';')[0])
-df['end_long'] = df['endLoc'].apply(lambda x: x.split(';')[1])
+df['start_long'] = df['startLoc'].apply(lambda x: x.split(';')[0])
+df['start_lat'] = df['startLoc'].apply(lambda x: x.split(';')[1])
+df['end_long'] = df['endLoc'].apply(lambda x: x.split(';')[0])
+df['end_lat'] = df['endLoc'].apply(lambda x: x.split(';')[1])
 
 # delete the startloc and endloc variables
-columns_to_keep = ['reservationNumber', 'from', 'to', 'passengers', 'rider', 
-					'serviceLevel', 'scheduledTime', 'start_lat','start_long',
-					'end_lat','end_long']
-df = df[columns_to_keep]
+columns_to_keep = ['reservationNumber', 'from', 'passengers', 'rider', 
+					'serviceLevel', 'scheduledTime', 'start_long','start_lat',
+					'end_long','end_lat']
+df = df[columns_to_keep].dropna()
+df['serviceLevel'].replace(to_replace = '571f94066095b2634daa3f7a', value = 'standard', inplace = True)
+df['serviceLevel'].replace(to_replace = '5720cacf9b36887aef7fbaa6', value = 'SUV/van', inplace = True)
 
 print df.head()
+print df['serviceLevel'].value_counts()
 
-
+'''
 # check the basic stat of each variable
 for i in columns_to_keep:
 	print i + ':\n'
 	print df[i].describe()
-	df[i].plot(kind = 'hist')
-	plt.show()
-	
+'''	
 
 
 
